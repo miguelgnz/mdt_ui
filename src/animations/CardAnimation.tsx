334010167474a97aa8aba9d805e4b60c258fd976
Miguel González <mgx.gon@gmail.com>
2024-05-07 23:01:27 -0600

    Animation on highoights section & fix on CardAnimation.tsx

--- src/animations/CardAnimation.tsx
+++ src/animations/CardAnimation.tsx
@@ -5,17 +5,23 @@ interface AnimatedWrapperProps {
 }
 
 const CardAnimation = ({ children }: AnimatedWrapperProps) => {
+  const spring = {
+    type: "spring",
+    damping: 10,
+    stiffness: 100,
+    duration: 0.4
+  };
   return (
     <motion.div
       initial={{ scale: 0.9, y: 10 }}
+      whileHover={{
+        scale: 1.01,
+        transition: { duration: 0.1, type: "spring" },
+      }}
       whileInView={{
         scale: 1,
         y: 0,
-        transition: { duration: 0.5, ease: "easeOut" },
-      }}
-      whileHover={{
-        scale: 1.01,
-        transition: { duration: 0.1, ease: "easeOut" },
+        transition: {spring}
       }}
     >
       {children}
