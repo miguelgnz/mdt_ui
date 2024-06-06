bbdec1244c790d9abfc6524a5385055591eda726
Miguel González <mgx.gon@gmail.com>
2024-06-05 23:18:49 -0600

    Setting some metatags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,29 @@ export default function Home() {
       <Head>
         <title>Estudio Mar de Tinta</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta name="description" content="Tattoo / Body Piercings" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://mdt-ui.vercel.app/" />
+        <meta property="og:title" content="Estudio Mar de Tinta" />
+        <meta property="og:description" content="Tattoo / Body Piercings" />
+        <meta
+          property="og:image"
+          content="https://metatags.io/images/meta-tags.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://mdt-ui.vercel.app/" />
+        <meta property="twitter:title" content="Estudio Mar de Tinta" />
+        <meta
+          property="twitter:description"
+          content="Tattoo / Body Piercings"
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/images/meta-tags.png"
+        />
       </Head>
       <HomePage />
     </>
