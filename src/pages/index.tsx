3e0e44f546a64f205fff576a919b04cd2425d167
Miguel González <mgx.gon@gmail.com>
2024-06-11 16:37:27 -0600

    Setting some metatags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,14 @@ export default function Home() {
       <Head>
         <title>Estudio Mar de Tinta</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
         <meta name="description" content="Tattoo / Body Piercings" />
+        <link rel="icon" href="/favicon.ico" />
+
+        <meta property="og:title" content="Estudio Mar de Tinta" />
+        <meta property="og:description" content="Tattoo / Body Piercings" />
+        <meta property="og:image" content="/mdt-logo.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.mardetinta.org" />
       </Head>
       <HomePage />
     </>
