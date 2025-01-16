c8c39f4c17dea6c98d991eaf69b2d8ce78e8ad1c
Miguel González <mgx.gon@gmail.com>
2025-01-16 08:24:37 -0600

    Separated LoginForm component from Modal

---
+++ src/components/Forms/LoginForm.tsx
@@ -0,0 +1,88 @@
+import { Box, Typography, Button, TextField, styled } from "@mui/material";
+import { useUser } from "@/context/UserContext";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { login } from "@/services/auth";
+
+type Props = {
+  setIsLoginModalOpen: (value: boolean) => void;
+};
+
+const LoginForm = ({ setIsLoginModalOpen }: Props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+
+  const { userLoading, setUserData, setIsUserLoading } = useUser();
+
+  const router = useRouter();
+
+  const onClickLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      setIsUserLoading(true);
+      const response = await login(email, password);
+
+      if (response.success) {
+        setUserData(response.data);
+        localStorage.setItem("isAuthenticated", "true");
+        router.push("/artist-profile");
+        setIsLoginModalOpen(false);
+      } else {
+        setError("Credenciales incorrectos");
+      }
+    } catch (error) {
+      setError("An unexpected error occurred. Please try again later."); // Fallback for unknown errors
+    } finally {
+      setIsUserLoading(false);
+    }
+  };
+  return (
+    <form onSubmit={onClickLogin} style={{ width: "100%" }}>
+      <Typography variant="h4" mb={2}>
+        Iniciar sesión
+      </Typography>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: "12px" }}>
+        <TextField
+          color="secondary"
+          variant="outlined"
+          label="Correo electrónico"
+          required
+          type="email"
+          onFocus={() => {
+            setError(null);
+          }}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          color="secondary"
+          variant="outlined"
+          required
+          type="password"
+          label="Contraseña"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        {error !== "" && <Typography color="red">{error}</Typography>}
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="success"
+          disabled={userLoading}
+        >
+          Ingresar
+        </Button>
+        <Typography variant="body2" textAlign="center">
+          ¿No tienes una cuenta?
+        </Typography>
+        <Button variant="outlined" color="secondary">
+          Registrarse
+        </Button>
+      </Box>
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,16 +1,8 @@
-import { useState, useEffect } from "react";
-import {
-  Modal,
-  Box,
-  Typography,
-  Button,
-  TextField,
-  styled,
-} from "@mui/material";
-import { login } from "@/services/auth";
-import { useRouter } from "next/router";
+import { Modal, styled } from "@mui/material";
+
 import { useUser } from "@/context/UserContext";
 import CustomLoader from "./CustomLoader";
+import LoginForm from "./Forms/LoginForm";
 
 type Props = {
   isLoginModalOpen: boolean;
@@ -26,49 +18,17 @@ const ModalContentWrapper = styled("div")(({ theme }) => ({
   width: "360px",
   backgroundColor: theme.palette.background.paper,
   borderRadius: "8px",
+  minHeight: "360px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 export default function LoginModal({
   isLoginModalOpen,
   setIsLoginModalOpen,
 }: Props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState<string | null>(null);
-
-  const router = useRouter();
-
-  const {
-    user,
-    isAuthenticated,
-    loginUser,
-    userLoading,
-    setUserData,
-    setIsUserLoading,
-  } = useUser();
-
-  const onClickLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    try {
-      setIsUserLoading(true);
-      const response = await login(email, password);
-
-      if (response.success) {
-        setUserData(response.data);
-        localStorage.setItem("isAuthenticated", "true");
-        router.push("/artist-profile");
-        setIsLoginModalOpen(false);
-      } else {
-        setError("Credenciales incorrectos");
-      }
-    } catch (error) {
-      setError("An unexpected error occurred. Please try again later."); // Fallback for unknown errors
-    } finally {
-      setIsUserLoading(false);
-    }
-  };
-
+  const { userLoading } = useUser();
 
   return (
     <Modal
@@ -82,51 +42,7 @@ export default function LoginModal({
           <CustomLoader color="secondary" />
         ) : (
           <>
-            <form onSubmit={onClickLogin}>
-              <Typography variant="h4" mb={2}>
-                Iniciar sesión
-              </Typography>
-              <Box
-                sx={{ display: "flex", flexDirection: "column", gap: "12px" }}
-              >
-                <TextField
-                  color="secondary"
-                  variant="outlined"
-                  label="Correo electrónico"
-                  required
-                  type="email"
-                  onFocus={() => {
-                    setError(null);
-                  }}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <TextField
-                  color="secondary"
-                  variant="outlined"
-                  required
-                  type="password"
-                  label="Contraseña"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-
-                {error !== "" && <Typography color="red">{error}</Typography>}
-
-                <Button
-                  type="submit"
-                  variant="contained"
-                  color="success"
-                  disabled={userLoading}
-                >
-                  Ingresar
-                </Button>
-                <Typography variant="body2" textAlign="center">
-                  ¿No tienes una cuenta?
-                </Typography>
-                <Button variant="outlined" color="secondary">
-                  Registrarse
-                </Button>
-              </Box>
-            </form>
+            <LoginForm setIsLoginModalOpen={setIsLoginModalOpen} />
           </>
         )}
       </ModalContentWrapper>
